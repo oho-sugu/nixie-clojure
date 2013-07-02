38b7889eddde5e55a08c1df856762e15477339fe
oho-s <oho-s@klab.com>
2013-07-02 11:50:55 +0900

    Initial Commit (lein new app nixie)

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to nixie
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/nixie/core_test.clj
@@ -0,0 +1,7 @@
+(ns nixie.core-test
+  (:require [clojure.test :refer :all]
+            [nixie.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
